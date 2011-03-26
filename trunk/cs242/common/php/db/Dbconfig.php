185649cbb30774ac301e92e84fe722610f96a701
yangcongknight@gmail.com <yangcongknight@gmail.com@ade0163f-310f-3e48-bf1c-be4b3e4d29b2>
2011-03-26 05:39:28 +0000

    Commited database module & test page

---
+++ trunk/cs242/common/php/db/Dbconfig.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Database configuration file
+ * configure database connection data
+ * @author Yaxing Chen
+ * @team SET
+ * @date 03/25/2010
+ */
+
+return array(
+	'host' => 'localhost',
+    'user' => 'root',
+	'pwd' => 'root',
+	'dbName' => '18902_CRM_Prod'
+);
+
+?>
---
+++ trunk/cs242/service/php/dbTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Database test & sample code
+ * 
+ * @author Yaxing Chen
+ * @team SET
+ * @date 03/26/2010
+ */
+
+#region include
+include_once '../../config.php';     
+#endregion include
+
+dbTest();
+
+/*
+ * database sample function
+ */
+function dbTest(){
+	
+	//connect using default username & password in Dbconfig.php
+	$db = new Database();
+	
+	//connect using designated username & password
+	//$db = new Database('18902_crmprod','crmprodcsci242');
+	
+	//create sql statement
+	//updates
+	$sql = "insert into Contact_type values('fax')";
+	$affectedRow = $db->update($sql);
+	
+	//print the number of affected rows
+	echo $affectedRow."</br>";
+	
+	//select
+	$sql = "select * from Contact_type";
+	$db->select($sql);
+	
+	//fetch result set as an indexed array 
+	//(also can fetch result set into other kinds of arrays, please refer to Database.class.php)
+	$result = $db->fetchIndexArray();
+	
+	//print all result in the format of fetched array
+	var_dump($result);
+	
+	//get & print number of rows in result set
+	echo "</br>".$db->numRows();
+	
+	//print the first element in the result set 
+	echo "</br>".$result[0][0];
+	
+	//delete
+	$sql = "delete from Contact_type where Contact_Type='fax'";
+	$affectedRow = $db->delete($sql);
+	echo "</br>".$affectedRow;
+}
+
+?>
