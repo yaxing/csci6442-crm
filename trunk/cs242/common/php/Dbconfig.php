dcbe6a501f1143f25dd9cc43bcd10636dc5269f7
yangcongknight@gmail.com <yangcongknight@gmail.com@ade0163f-310f-3e48-bf1c-be4b3e4d29b2>
2011-04-04 04:19:50 +0000

    modify database.class.php & Dbconfig.php

--- trunk/cs242/common/php/Dbconfig.php
+++ trunk/cs242/common/php/Dbconfig.php
@@ -8,11 +8,6 @@
  */
 
 return array(
-//	'host' => 'localhost',
-//    'user' => 'root',
-//	'pwd' => 'root',
-//	'dbName' => '18902_CRM_Prod'
-
 	'host' => 'mysql2.myregisteredsite.com',
     'user' => '175192_crmtest',
 	'pwd' => 'crmtestcsci242',
