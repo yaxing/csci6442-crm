0fe0376d0a9a7627686334c2eeb658e482f07508
Kevin.ltyou@gmail.com <Kevin.ltyou@gmail.com@ade0163f-310f-3e48-bf1c-be4b3e4d29b2>
2011-04-05 20:45:13 +0000

    sales team : add communication, login; modify adding account

--- cs242/sales/flex/Dbconfig.php
+++ cs242/sales/flex/Dbconfig.php
@@ -8,15 +8,15 @@
  */
 
 return array(
-//	'host' => 'localhost',
+	'host' => 'localhost',//'host' => 'mysql2.myregisteredsite.com',
 //    'user' => 'root',
 //	'pwd' => 'root',
 //	'dbName' => '18902_CRM_Prod'
 
-	'host' => '209.237.150.136',
-    'user' => '18902_crmtest',
-	'pwd' => 'crmtestcsci242',
-	'dbName' => '18902_CRM_Test'
+	//'host' => '209.237.150.136',
+    //'user' => '18902_crmtest',
+	//'pwd' => 'crmtestcsci242',
+	//'dbName' => '18902_CRM_Test'	'user' => '175192_crmtest',	'pwd' => 'crmtestcsci242',	'dbName' => '175192_CRM_Test'
 );
 
 ?>
