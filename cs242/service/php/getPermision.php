35f2eff668c46d200d4ad764988e732034740c9f
krista.bacungan@gmail.com <krista.bacungan@gmail.com@ade0163f-310f-3e48-bf1c-be4b3e4d29b2>
2011-04-13 02:59:34 +0000

    Deliverables for Demo 3

---
+++ cs242/service/php/getPermision.php
@@ -0,0 +1,30 @@
+<?php
+	
+	/**
+	 * Get all permissions
+	 * 
+	 * @author Alex Florescu
+	 * @team Services
+	 * @date 04/02/2011
+	 */
+	
+	
+	#region include
+	include_once 'header.php';
+	#endregion include
+	$available_to=$_POST['available_to'];
+	
+	//execute statement
+	$sql = "SELECT * FROM permission WHERE available_to=$available_to;";
+	
+	$db->select($sql);
+	$result = $db->fetchAssoc();
+	
+	foreach ($result as $index => $row) {
+		echo "<permission>\n";
+		foreach ($row as $column => $value)
+		echo "<$column>$value</$column>\n";
+		echo "</permission>\n\n";
+	}
+	
+	?>
